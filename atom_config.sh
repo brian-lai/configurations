54ae7b05348c7f79a17dcaccac475bd0bd26c183
brian lai <brian.lai.1992@gmail.com>
2021-05-08 16:06:36 -0400

    apm install stuff

---
+++ atom_config.sh
@@ -0,0 +1,30 @@
+apm install linter # Base linter
+apm install linter-ruby
+apm install linter-scss-lint
+apm install linter-coffeelint
+apm install linter-rubocop
+apm install linter-haml
+# nicer code
+apm install atom-beautify
+apm install atom-css-comb
+# testing
+apm install ruby-test
+apm install cucumber
+apm install cucumber-step # open the step definition
+# language highlighting
+apm install language-rspec
+apm install language-haml
+apm install language-docker
+# other
+apm install minimap # Shows you a tiny preview of the file on the right
+apm install Sublime-Style-Column-Selection # Allows you to select columns
+apm install platformio-ide-terminal # terminal inside your editor
+# columns
+apm install toggle-quotes
+apm install trailing-spaces
+# for extra credit
+apm install color-picker
+apm install git-blame
+# vim
+apm install vim-mode-plus
+apm install ex-mode
